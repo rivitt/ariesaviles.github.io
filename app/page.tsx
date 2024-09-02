9c695930012a91aade8f9fb73c1a5ee13a13fd66
aries <44047715+ariesaviles@users.noreply.github.com>
2024-09-01 22:42:58 -0400

    mobile & desktop

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,18 @@ export default function Home() {
 
       {/* hero */}
       <div className="flex flex-col lg:flex-row-reverse w-full h-[calc(100vh-5rem)] items-center bg-green-600 space-y-10">
-        <div className="relative flex justify-center items-center lg:justify-end w-full h-2/4 bg-yellow-400 transform translate-y-20">
-          {/* <!-- Adjust z-index to layer behind hero text --> */}
-          <div className="absolute flex w-full lg:w-[40rem] bg-orange-400 h-40 rounded-2xl z-10" />
-          <div className="absolute flex justify-center bg-gray-200 w-48 h-64 rounded-full z-30" />
-          <div className="absolute flex justify-center bg-yellow-400 w-56 h-72 rounded-full z-20" />
+        <div className="relative justify-center items-start lg:justify-end lg:items-center w-full h-2/4 bg-yellow-400 transform translate-y-40 lg:translate-y-0">
+          <div className="absolute flex w-full items-center lg:w-[125%] bg-orange-400 h-40 rounded-2xl lg:right-0 lg:top-1/2 lg:transform lg:-translate-y-1/2 z-10">
+            <div className="absolute left-1/2 transform -translate-x-1/2 bg-gray-200 w-48 h-64 rounded-full z-30" />
+            <div className="absolute left-1/2 transform -translate-x-1/2 bg-yellow-400 w-56 h-72 rounded-full z-20" />
+          </div>
         </div>
-        <div className="relative flex w-full h-2/5 justify-center bg-white text-2xl">
+        <div className="relative flex w-full h-2/5 lg:h-full justify-center text-2xl">
           {/* <!-- Hero text section with higher z-index to be on top --> */}
-          <div className="absolute text-center lg:text-left w-full flex flex-col leading-19 z-30">
+          <div className="absolute flex flex-col text-center lg:justify-center lg:text-left w-full lg:h-4/5 leading-19 z-30">
             <p className="text-4xl my-2">I&apos;m Aries,</p>
             <p>a Software Engineer & Mentor.</p>
-            <p>I enjoy bringing dreams to life.</p>
+            <p>tag line here</p>
           </div>
           {/* <!-- "SCROLL" section with higher z-index to be on top --> */}
           <div className="absolute bottom-0 w-full h-fit flex flex-col items-center pt-10 space-y-2 z-30">
